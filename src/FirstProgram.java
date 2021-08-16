e6c0eafff677b8c6d073e33456340a2c8fb6a383
Dan <Dan@DanScheiner.com>
2021-08-16 14:36:08 -0400

    Week 1 Finished

---
+++ src/FirstProgram.java
@@ -0,0 +1,13 @@
+// First Program in Java
+// Daniel Scheiner
+
+public class FirstProgram
+{
+    public static void main ( String [ ] args )
+    {
+        System.out.println( "Programming is not a spectator sport!" );
+        System.out.println( "Daniel Scheiner is one of its players.");
+
+        System.exit( 0 );
+    }
+}
